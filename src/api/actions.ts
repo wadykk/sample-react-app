95701667d10d04d62e3071b204c26f7596790e86
wadykk <158468806+wadykk@users.noreply.github.com>
2024-04-27 01:01:55 +0000

    changed api

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://localhost:3000/api";
+const API_URL = "https://laughing-fortnight-4jjxgvxrvjxxcr-3000.app.github.dev/";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
